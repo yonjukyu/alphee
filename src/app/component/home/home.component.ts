ee6aa241afa89d0ed064c0887a050475f9ed0115
HUGOD <yonjukyu2@gmail.com>
2023-05-16 14:23:25 +0200

    remove collection description

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -9,9 +9,9 @@ import {CarouselProduct} from "../../../models/carouselProduct";
 export class HomeComponent implements OnInit{
 
   carouselProducts: CarouselProduct[] = [
-    new CarouselProduct("Nuit torride","../assets/Body Rouge Lisa (3).jpg","COLLECTION 2 DESCRIPTION"),
-    new CarouselProduct("Lune de miel","../assets/couple.jpg","COLLECTION 2 DESCRIPTION"),
-    new CarouselProduct("Sombre Désir","../assets/arthemis (3).png","COLLECTION 1 DESCRIPTION"),
+    new CarouselProduct("Nuit torride","../assets/Body Rouge Lisa (3).jpg",""),
+    new CarouselProduct("Lune de miel","../assets/couple.jpg",""),
+    new CarouselProduct("Sombre Désir","../assets/arthemis (3).png",""),
   ]
 
   ngOnInit(): void {
